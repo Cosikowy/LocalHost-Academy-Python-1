fe3292316686bc901bfaa665ae2f688b43fa1758
Cosikowy <mr.cosik@gmail.com>
2020-01-16 14:43:39 +0100

    last task

---
+++ zadania/task10.py
@@ -0,0 +1,7 @@
+# wygeneruj 5 elementową listę, której elementami będą listy 100 losowych liczb
+# funkcja powinna zwracać zmodyfikowane listy:
+# lista z indexem 0 i 1 pozostaje nie znieniona,
+# lista z indexem 2 ma co drugi element zamieniony na "a"
+# lista z indexem 3 ma co trzeci element zamieniony na "b"
+# lista z indexem 4 ma co czwarty element zamieniony na "c"
+# lista z indexem 5 ma co piąty element zamieniony na "d"
